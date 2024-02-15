a8e7383bf1f8fa3889c6265b673ea699550c8803
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2024-02-15 19:21:27 +0200

    Adding loading page

--- js/script.js
+++ js/script.js
@@ -6,68 +6,91 @@ const domElements = {
   buttons: document.querySelectorAll(".tab-button"),
   tabs: document.querySelectorAll(".tabs > div"),
   bodyElement: document.body,
-  imageElement: document.querySelector(".home .container .image img")
+  imageElement: document.querySelector(".home .container .image img"),
 };
 
-// Get the dark mode status from localStorage when the page loads
 document.addEventListener("DOMContentLoaded", () => {
-  let isDarkMode = localStorage.getItem('darkMode') === 'true';
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && (isDarkMode || localStorage.getItem('darkMode') === null )) {
+  let isDarkMode = localStorage.getItem("darkMode") === "true";
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches &&
+    (isDarkMode || localStorage.getItem("darkMode") === null)
+  ) {
     isDarkMode = true;
   }
   if (isDarkMode) {
     domElements.bodyElement.classList.add("dark-mode");
-    domElements.imageElement.setAttribute("src", "images/AbubakrAlsheikhDark.jpeg");
+    domElements.imageElement.setAttribute(
+      "src",
+      "images/AbubakrAlsheikhDark.jpeg"
+    );
   }
+
+  document.getElementById("overlay").classList.add("hide");
+  setTimeout(() => {
+    document.getElementById("overlay").style.display = "none";
+  }, 1000);
+
+  // Intersection Observer
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  });
+  domElements.hiddenElements.forEach((element) => observer.observe(element));
 });
 
 // Scroll event listener
-window.addEventListener("scroll", () => {
-  domElements.header.classList.toggle("scroll", window.scrollY > 50);
-  setActiveLink();
-}, { passive: true });
+window.addEventListener(
+  "scroll",
+  () => {
+    domElements.header.classList.toggle("scroll", window.scrollY > 50);
+    setActiveLink();
+  },
+  { passive: true }
+);
 
 // Active link setting
 function removeActiveClass() {
-  domElements.links.forEach(link => link.classList.remove("active"));
+  domElements.links.forEach((link) => link.classList.remove("active"));
 }
 function setActiveLink() {
   const scrollPos = window.screenY || document.documentElement.scrollTop + 200;
-  domElements.links.forEach(link => {
+  domElements.links.forEach((link) => {
     const target = document.querySelector(link.getAttribute("href"));
-    if (target.offsetTop <= scrollPos && target.offsetTop + target.offsetHeight > scrollPos) {
+    if (
+      target.offsetTop <= scrollPos &&
+      target.offsetTop + target.offsetHeight > scrollPos
+    ) {
       removeActiveClass();
       link.classList.add("active");
     }
   });
 }
 
-// Intersection Observer
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-});
-domElements.hiddenElements.forEach(element => observer.observe(element));
-
 function toggleDarkMode() {
   domElements.bodyElement.classList.toggle("dark-mode");
   const isDarkMode = domElements.bodyElement.classList.contains("dark-mode");
-  
-  domElements.imageElement.setAttribute("src", isDarkMode ? "images/AbubakrAlsheikhDark.jpeg" : "images/AbubakrAlsheikh.jpeg");
-  
+
+  domElements.imageElement.setAttribute(
+    "src",
+    isDarkMode
+      ? "images/AbubakrAlsheikhDark.jpeg"
+      : "images/AbubakrAlsheikh.jpeg"
+  );
+
   // Save the dark mode status to localStorage
-  localStorage.setItem('darkMode', isDarkMode);
+  localStorage.setItem("darkMode", isDarkMode);
 }
 
 // Tab switching
 document.addEventListener("DOMContentLoaded", () => {
   domElements.buttons.forEach((button, index) => {
     button.addEventListener("click", () => {
-      domElements.buttons.forEach(btn => btn.classList.remove("active"));
-      domElements.tabs.forEach(tab => tab.classList.remove("active"));
+      domElements.buttons.forEach((btn) => btn.classList.remove("active"));
+      domElements.tabs.forEach((tab) => tab.classList.remove("active"));
       button.classList.add("active");
       domElements.tabs[index].classList.add("active");
     });
