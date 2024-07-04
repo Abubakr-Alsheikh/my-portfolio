5c3ae62426aff15b219439a5864424e0147bdd82
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2024-07-04 21:56:39 +0300

    Change the data as array other than using model

--- js/handleData.js
+++ js/handleData.js
@@ -1,8 +1,408 @@
-import projects from "../data/projects.json" assert { type: "json" };
-import certificates from "../data/certificates.json" assert { type: "json" };
-import education from "../data/education.json" assert { type: "json" };
-import skills from "../data/skills.json" assert { type: "json" };
-import platforms from "../data/platforms.json" assert { type: "json" };
+const projects = [
+  {
+    id: 1,
+    title: "lona Template",
+    "image-project": "images/Projects/Projects Pictures/lona.jpg",
+    "image-laptop": "images/Projects/Lona Preview.jpg",
+    url: "https://abubakr-alsheikh.github.io/leon-website/",
+    type: "other",
+  },
+  {
+    id: 2,
+    title: "kasper Template",
+    "image-project": "images/Projects/Projects Pictures/kasperTemp.jpg",
+    "image-laptop": "images/Projects/kasper Preview.jpg",
+    url: "https://abubakr-alsheikh.github.io/kasper-website/",
+    type: "other",
+  },
+  {
+    id: 3,
+    title: "Elzero Template",
+    "image-project": "images/Projects/Projects Pictures/ElzeroTemp.jpg",
+    "image-laptop": "images/Projects/Elzero Preview.jpg",
+    url: "https://abubakr-alsheikh.github.io/elzero-template-website",
+    type: "other",
+  },
+  {
+    id: 4,
+    title: "Movie website",
+    "image-project": "images/Projects/Projects Pictures/MovieSite.jpg",
+    "image-laptop": "images/Projects/MovieSite Preview.jpg",
+    url: "https://abubakr-alsheikh.github.io/movie-website/",
+    type: "other",
+  },
+  {
+    id: 5,
+    title: "sama media",
+    "image-project": "images/Projects/Projects Pictures/samaMedia.jpg",
+    "image-laptop": "images/Projects/SamaMedia Preview.jpg",
+    url: "https://sama-media.infinityfreeapp.com/",
+    type: "top",
+  },
+  {
+    id: 6,
+    title: "Dashboard website",
+    "image-project": "images/Projects/Projects Pictures/ElzeroDashboard.png",
+    "image-laptop": "images/Projects/ElzeroDashboard Preview.jpg",
+    url: "https://abubakr-alsheikh.github.io/dashboard-website",
+    type: "other",
+  },
+  {
+    id: 7,
+    title: "ReactJS Blog",
+    "image-project": "images/Projects/Projects Pictures/ReactJS blog.jpeg",
+    "image-laptop": "images/Projects/ReactJS blog.jpg",
+    url: "https://delicate-macaron-e1f854.netlify.app/",
+    type: "other",
+  },
+  {
+    id: 8,
+    title: "Online book inventory system",
+    "image-project": "images/Projects/Projects Pictures/onlineBook.jpeg",
+    "image-laptop": "images/Projects/Online book inventory system.jpg",
+    url: "https://abubakralsheikh.bsite.net/",
+    type: "top",
+  },
+  {
+    id: 9,
+    title: "E-commerce webiste",
+    "image-project": "images/Projects/Projects Pictures/Ecommerce webiste.jpeg",
+    "image-laptop": "images/Projects/Ecommerce webiste Preview.jpg",
+    url: "https://abubakralsheikh.pythonanywhere.com/",
+    type: "top",
+  },
+  {
+    id: 10,
+    title: "Modern Bank",
+    "image-project": "images/Projects/Projects Pictures/modern bank.jpeg",
+    "image-laptop": "images/Projects/modern bank.png",
+    url: "https://abubakr-alsheikh.github.io/bank_modern_website/",
+    type: "other",
+  },
+  {
+    id: 11,
+    title: "AskVid: get answers from videos and audio",
+    "image-project": "images/Projects/Projects Pictures/AskVid.jpeg",
+    "image-laptop": "images/Projects/AskVid.jpg",
+    url: "https://abubakralsheikh.pythonanywhere.com/askvid/",
+    type: "top",
+  },
+];
+const certificates = [
+  {
+    id: 1,
+    title: "html",
+    image: "images/Certificates/Web design for everybody/HTML.jpeg",
+    url: "https://coursera.org/verify/8S9XQWSE88KB",
+    type: "other",
+  },
+  {
+    id: 2,
+    title: "css",
+    image: "images/Certificates/Web design for everybody/CSS.jpeg",
+    url: "https://coursera.org/verify/4HSBD74W3G4Z",
+    type: "other",
+  },
+  {
+    id: 3,
+    title: "advanced css",
+    image: "images/Certificates/Web design for everybody/Advanced CSS.jpeg",
+    url: "https://coursera.org/verify/A9KWRLGVX4TU",
+    type: "other",
+  },
+  {
+    id: 4,
+    title: "JavaScript",
+    image: "images/Certificates/Web design for everybody/JavaScript.jpeg",
+    url: "https://coursera.org/verify/UFXA6LEWMAKK",
+    type: "other",
+  },
+  {
+    id: 5,
+    title: "Web Design for Everybody Capstone",
+    image:
+      "images/Certificates/Web design for everybody/Web Design for Everybody Capstone.jpg",
+    url: "https://coursera.org/verify/N9FUPRBZNUHW",
+    type: "other",
+  },
+  {
+    id: 6,
+    title: "Webdesign for everybody",
+    image:
+      "images/Certificates/Web design for everybody/Webdesign for everybody.jpeg",
+    url: "https://coursera.org/verify/specialization/UERU9T5YRQWS",
+    type: "top",
+  },
+  {
+    id: 7,
+    title: "Building Web Applications in PHP",
+    image:
+      "images/Certificates/Web Applications for Everybody/Building Web Applications in PHP.jpg",
+    url: "https://coursera.org/verify/9MW68H78WDKS",
+    type: "other",
+  },
+  {
+    id: 8,
+    title: "Structured Query Language",
+    image:
+      "images/Certificates/Web Applications for Everybody/Introduction to Structured Query Language.jpg",
+    url: "https://coursera.org/verify/BNBWX3J4S8V4",
+    type: "other",
+  },
+  {
+    id: 9,
+    title: "Building Database Applications",
+    image:
+      "images/Certificates/Web Applications for Everybody/Building Database Applications in PHP.jpeg",
+    url: "https://coursera.org/verify/F84744HRCDRL",
+    type: "other",
+  },
+  {
+    id: 10,
+    title: "JavaScript, jQuery, and JSON",
+    image:
+      "images/Certificates/Web Applications for Everybody/JavaScript, jQuery, and JSON.jpg",
+    url: "https://coursera.org/verify/SCNXSMBPQAFR",
+    type: "other",
+  },
+  {
+    id: 11,
+    title: "Web Applications for Everybody",
+    image:
+      "images/Certificates/Web Applications for Everybody/Web Applications for Everybody.jpg",
+    url: "https://coursera.org/verify/specialization/F7RBLWC5AGB3",
+    type: "top",
+  },
+  {
+    id: 12,
+    title: "Getting Started with Python",
+    image:
+      "images/Certificates/Python for Everybody/Programming for Everybody.jpg",
+    url: "https://coursera.org/verify/RH57HCNB8FP7",
+    type: "other",
+  },
+  {
+    id: 13,
+    title: "Python Data Structures",
+    image:
+      "images/Certificates/Python for Everybody/Python Data Structures.png",
+    url: "https://coursera.org/verify/2RUMELQB6PBZ",
+    type: "other",
+  },
+  {
+    id: 14,
+    title: "Using Python to Access Web Data",
+    image:
+      "images/Certificates/Python for Everybody/Using Python to Access Web Data.png",
+    url: "https://coursera.org/verify/N2657JBVMH8A",
+    type: "other",
+  },
+  {
+    id: 15,
+    title: "Using Databases with Python",
+    image:
+      "images/Certificates/Python for Everybody/Using Databases with Python.png",
+    url: "https://coursera.org/verify/SAR286WGSPVY",
+    type: "other",
+  },
+  {
+    id: 16,
+    title: "Capstone: R.P.V Data with Python",
+    image:
+      "images/Certificates/Python for Everybody/Capstone Retrieving, Processing, and Visualizing.png",
+    url: "https://coursera.org/verify/VBQTN7HY99VE",
+    type: "other",
+  },
+  {
+    id: 17,
+    title: "Python for Everybody",
+    image: "images/Certificates/Python for Everybody/Python for Everybody.png",
+    url: "https://coursera.org/verify/specialization/QTNT2MHPT8M7",
+    type: "top",
+  },
+  {
+    id: 18,
+    title: "Technical diploma",
+    image: "images/Certificates/Technical diploma certificate.jpg",
+    url: "",
+    type: "top",
+  },
+  {
+    id: 24,
+    title: "EF Standard English Test",
+    image: "images/Certificates/EF SET Certificate.jpeg",
+    url: "https://efset.org/cert/T7x6T7",
+    type: "top",
+  },
+  {
+    id: 19,
+    title: "Web Application Technologies and Django",
+    image:
+      "images/Certificates/Django for Everybody/Web Application Technologies and Django.jpeg",
+    url: "https://coursera.org/verify/NMRHG7J6BHTY",
+    type: "other",
+  },
+  {
+    id: 20,
+    title: "Building Web Applications in Django",
+    image:
+      "images/Certificates/Django for Everybody/Building Web Applications in Django.jpeg",
+    url: "https://coursera.org/verify/6EHD4UUNY9XS",
+    type: "other",
+  },
+  {
+    id: 21,
+    title: "Django Features and Libraries",
+    image:
+      "images/Certificates/Django for Everybody/Django Features and Libraries.jpeg",
+    url: "https://coursera.org/verify/RJDJF6NDG9A6",
+    type: "other",
+  },
+  {
+    id: 22,
+    title: "Using JavaScript and JSON in Django",
+    image:
+      "images/Certificates/Django for Everybody/Using JavaScript and JSON in Django.jpeg",
+    url: "https://coursera.org/verify/2X26USBKJN6G",
+    type: "other",
+  },
+  {
+    id: 23,
+    title: "Django for Everybody",
+    image: "images/Certificates/Django for Everybody/Django for Everybody.jpeg",
+    url: "https://coursera.org/verify/specialization/DC49ZLKAM5TQ",
+    type: "top",
+  },
+];
+const education = [
+  {
+    id: 1,
+    title: "Software engineering",
+    place: "Aleppo University | Technical Diploma",
+    date: "2021 - 2023",
+    degree: "90.36% | 1st graduate",
+  },
+  {
+    id: 2,
+    title: "information technology engineering",
+    place: "Syrian Virtual University | Bachelor's",
+    date: "2021 - 2026",
+    degree: "",
+  },
+  {
+    id: 3,
+    title: "Software engineering",
+    place: "Aleppo University | Bachelor's",
+    date: "2023 - 2028",
+    degree: "",
+  },
+];
+const skills = {
+  "Front End": [
+    {
+      skill: "HTML",
+      prograss: "95%",
+    },
+    {
+      skill: "CSS",
+      prograss: "90%",
+    },
+    {
+      skill: "JavaScript",
+      prograss: "90%",
+    },
+    {
+      skill: "Bootstrap",
+      prograss: "80%",
+    },
+    {
+      skill: "Tailwind",
+      prograss: "65%",
+    },
+    {
+      skill: "React",
+      prograss: "70%",
+    },
+  ],
+  "Back End": [
+    {
+      skill: "Django",
+      prograss: "70%",
+    },
+    {
+      skill: "PHP",
+      prograss: "80%",
+    },
+    {
+      skill: "Asp.net",
+      prograss: "65%",
+    },
+  ],
+  "Data Bases": [
+    {
+      skill: "MySQL",
+      prograss: "70%",
+    },
+    {
+      skill: "MS SQL",
+      prograss: "75%",
+    },
+    {
+      skill: "SQLite",
+      prograss: "60%",
+    },
+  ],
+  "Design tools": [
+    {
+      skill: "Figma",
+      prograss: "80%",
+    },
+    {
+      skill: "Photoshop",
+      prograss: "70%",
+    },
+  ],
+  "Programming languages": [
+    {
+      skill: "C#",
+      prograss: "80%",
+    },
+    {
+      skill: "Python",
+      prograss: "70%",
+    },
+    {
+      skill: "C++",
+      prograss: "65%",
+    },
+  ],
+  Other: [
+    {
+      skill: "AI Tools",
+      prograss: "70%",
+    },
+    {
+      skill: "Microsoft Office",
+      prograss: "80%",
+    },
+    {
+      skill: "Unity",
+      prograss: "60%",
+    },
+  ],
+};
+const platforms = [
+  {
+    title: "Khmasat",
+    "url-profile": "https://khamsat.com/user/abubakr_alsheikh",
+    img: "https://th.bing.com/th/id/R.20b169cfe52d1ed3ceb2e830ba764cd1?rik=IEOwyIkFY72BBQ&pid=ImgRaw&r=0",
+  },
+  {
+    title: "mostaql",
+    "url-profile": "https://mostaql.com/u/AbubakrAlshiekh/portfolio",
+    img: "https://th.bing.com/th/id/OIP.nmDkft987K348N200Tau7QAAAA?pid=ImgDet&rs=1",
+  },
+];
 
 window.addEventListener("DOMContentLoaded", () => {
   loadData(projects, showProjects, ".projects", "top");
@@ -13,23 +413,25 @@ window.addEventListener("DOMContentLoaded", () => {
   updateBoxFillter();
 });
 
-function updateBoxFillter(){
+function updateBoxFillter() {
   // const boxes = document.querySelectorAll('.projects .container .box');
-  const boxes = document.querySelectorAll('.projects .container .box, .certificates .container .box');
-        
-  window.addEventListener('scroll', function() {
-    boxes.forEach(box => {
+  const boxes = document.querySelectorAll(
+    ".projects .container .box, .certificates .container .box"
+  );
+
+  window.addEventListener("scroll", function () {
+    boxes.forEach((box) => {
       const boxRect = box.getBoundingClientRect();
       const windowHeight = window.innerHeight;
 
       // Check if the box is in the middle of the viewport
-      const isCentered = boxRect.top <= (windowHeight / 2) && 
-                        boxRect.bottom >= (windowHeight / 2);
+      const isCentered =
+        boxRect.top <= windowHeight / 2 && boxRect.bottom >= windowHeight / 2;
 
       if (isCentered) {
-        box.querySelector('img').style.filter = 'grayscale(0)';
+        box.querySelector("img").style.filter = "grayscale(0)";
       } else {
-        box.querySelector('img').style.filter = 'grayscale(1)';
+        box.querySelector("img").style.filter = "grayscale(1)";
       }
     });
   });
